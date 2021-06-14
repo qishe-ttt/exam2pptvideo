8479c7402792785613b40863c84078f54100732c
JingWeiLyu <murka@JingWeiLyus-MBP.powter>
2021-06-14 11:10:41 +0800

    update templates

--- exam2pptvideo/de/vocab_phrase_ppt.py
+++ exam2pptvideo/de/vocab_phrase_ppt.py
@@ -18,8 +18,8 @@ class GermanVocabPhrasePPT(VocabPhrasePPT):
 
   _score_code = {
     "100": ["PERFEKT", "全对了，很棒哦！去下一个等级挑战试试吧"],
-    "60-90": ["SEHR GUT", "还不错哦，八啾觉得\"多浸泡在语言环境中可以潜移默化的提高语言能\""],
-    "0-50": ["SO LALA", "没能及格有些遗憾，八啾觉得\"刷题虽苦但有用，基础扎实了才能展翅飞翔\""]
+    "60-90": ["SEHR GUT", "还不错哦，八啾觉得\n\"多浸泡在语言环境中可以潜移默化的提高语言能\""],
+    "0-50": ["SO LALA", "没能及格有些遗憾，八啾觉得\n\"刷题虽苦但有用，基础扎实了才能展翅飞翔\""]
   }
   
   _cal_slogan = "Ihre Punktzahl wird nun berechnet."
